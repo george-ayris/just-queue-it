87c38b4c60260004f13b6d3631a69c58bc2b4164
George Ayris <george.ayris@huddle.com>
2014-10-07 16:09:13 +0100

    Got basic jumbotron working and removed unnecessary css

--- app/assets/javascripts/viewmodels/shell.js
+++ app/assets/javascripts/viewmodels/shell.js
@@ -1,14 +1,9 @@
 define(['plugins/router', 'durandal/app'], function (router, app) {
     return {
         router: router,
-        search: function() {
-            //It's really easy to show a message box.
-            //You can add custom options too. Also, it returns a promise for the user's response.
-            app.showMessage('Search not yet implemented...');
-        },
         activate: function () {
             router.map([
-                { route: '', title: 'Welcome', moduleId: 'viewmodels/welcome', nav: true  },
+                { route: ['', 'about'], title: 'About', moduleId: 'viewmodels/about', nav: true  },
                 { route: 'simple', moduleId: 'viewmodels/simple', nav: true }
             ]).buildNavigationModel();
 
